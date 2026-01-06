60ab4b131efeacd9e120746f3771bafd1682510f
Arpit Srivastava <arpvastava@gmail.com>
2026-01-06 20:21:28 +0530

    Add player movement

--- src/dom/classes/Player.tsx
+++ src/dom/classes/Player.tsx
@@ -4,6 +4,10 @@ export class Player {
     player: Mesh | null = null
     scene: Scene
 
+    moveDistance: number = 2
+    targetX: number = 0
+    speed: number = 10
+
     constructor(scene: Scene) {
         this.scene = scene
     }
@@ -11,21 +15,29 @@ export class Player {
     setup() {
         const geometry = new BoxGeometry(1, 1, 1)
         const material = new MeshBasicMaterial({ color: "#ff6969" })
-        
+
         this.player = new Mesh(geometry, material)
         this.player.position.set(0, 0, 0)
-        
+
         this.scene.add(this.player)
+
+        // Add player movement
+        window.addEventListener("keydown", this.onKeyPress)
     }
 
-    update(_delta: number) {
+    update(delta: number) {
         if (!this.player)
             return
+
+        this.player.position.x += (this.targetX - this.player.position.x) * this.speed * delta
     }
 
     destroy() {
         if (!this.player)
             return
+
+        // Remove event listeners
+        window.removeEventListener("keydown", this.onKeyPress)
 
         // Clear main reference beforehand
         const p = this.player
@@ -43,5 +55,26 @@ export class Player {
 
         // Remove from scene
         this.scene.remove(p)
+    }
+
+    private onKeyPress = (e: KeyboardEvent) => {
+        const key = e.key
+
+        if (key === "a" || key === "ArrowLeft") {
+            if (this.targetX === this.moveDistance) {
+                this.targetX = 0
+            }
+            else if (this.targetX === 0) {
+                this.targetX = -this.moveDistance
+            }
+        }
+        else if (key === "d" || key === "ArrowRight") {
+            if (this.targetX === -this.moveDistance) {
+                this.targetX = 0
+            }
+            else if (this.targetX === 0) {
+                this.targetX = this.moveDistance
+            }
+        }
     }
 }
